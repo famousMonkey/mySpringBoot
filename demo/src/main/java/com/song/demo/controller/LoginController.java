ab0cdd5050e88ff309f1a6aec3c95e71897b230b
monkey <13838121279@163.com>
2019-05-30 16:52:44 +0800

    测试springboot整合redis

--- demo/src/main/java/com/song/demo/controller/LoginController.java
+++ demo/src/main/java/com/song/demo/controller/LoginController.java
@@ -1,17 +1,26 @@
 package com.song.demo.controller;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
-@RestController
+@Controller
 @RequestMapping(value = "login")
 public class LoginController {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @ResponseBody
     @GetMapping(value = "/index")
     public String Login(HttpServletRequest request){
         String name = request.getParameter("name");
@@ -19,4 +28,27 @@ public class LoginController {
         log.info("password:"+password);
         return "Hello "+name;
     }
+
+    @GetMapping(value = "redis")
+    public String myRedis(){
+        String name="monkey";
+        String s = JSON.toJSONString(name);
+        stringRedisTemplate.opsForValue().set("wwwsssaaaddd",s,30, TimeUnit.SECONDS);
+        return "success";
+    }
+
+    @GetMapping(value = "getvalue")
+    public String getRedisKey(){
+        log.info("我被调用了。。。");
+        String value = stringRedisTemplate.opsForValue().get("wwwsssaaaddd");
+        String s = JSON.parseObject(value, String.class);
+        if(s!=null){
+            return "index";
+        }else{
+            return "fail";
+        }
+    }
+
+
+
 }
