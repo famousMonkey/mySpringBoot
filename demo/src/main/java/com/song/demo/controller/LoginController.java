07c7c032516ff9bd9eb10977b388beb547185f54
monkey <13838121279@163.com>
2019-05-30 09:26:07 +0800

    控制器跳转

--- demo/src/main/java/com/song/demo/controller/LoginController.java
+++ demo/src/main/java/com/song/demo/controller/LoginController.java
@@ -1,22 +1,30 @@
 package com.song.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
-@RestController
+@Controller
 @RequestMapping(value = "login")
 public class LoginController {
 
-    @GetMapping(value = "/index")
+    @GetMapping(value = "index")
+    public String toLogin(){
+        return "index";
+    }
+
+    @GetMapping(value = "/index_1")
     public String Login(HttpServletRequest request){
         String name = request.getParameter("name");
         String password = request.getParameter("password");
-        log.info("password:"+password);
-        return "Hello "+name;
+        request.setAttribute("userName",name);
+        HttpSession session = request.getSession();
+        session.setAttribute("pwd",password);
+        return "success";
     }
 }
