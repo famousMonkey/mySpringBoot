f06d48d54d6fc92823a73943f0ddb68900992f00
monkey <13838121279@163.com>
2019-05-30 08:47:44 +0800

    获取表单提交的参数

--- demo/src/main/java/com/song/demo/controller/LoginController.java
+++ demo/src/main/java/com/song/demo/controller/LoginController.java
@@ -1,15 +1,22 @@
 package com.song.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestController
 @RequestMapping(value = "login")
 public class LoginController {
 
     @GetMapping(value = "/index")
-    public String Login(){
-        return "SUCCESS";
+    public String Login(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        log.info("password:"+password);
+        return "Hello "+name;
     }
 }
