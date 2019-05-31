370ce769d695d7ec8d1f48c0b270df1c5eccfc95
monkey <13838121279@163.com>
2019-05-31 15:14:34 +0800

    前后端交互小试牛刀

---
+++ demo/src/main/java/com/song/demo/constant/Result.java
@@ -0,0 +1,9 @@
+package com.song.demo.constant;
+
+import lombok.Data;
+
+@Data
+public class Result {
+    private Boolean success;
+    private String message;
+}
--- demo/src/main/java/com/song/demo/controller/LoginController.java
+++ demo/src/main/java/com/song/demo/controller/LoginController.java
@@ -1,13 +1,13 @@
 package com.song.demo.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.song.demo.constant.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.TimeUnit;
@@ -29,7 +29,38 @@ public class LoginController {
         return "Hello "+name;
     }
 
-    @GetMapping(value = "redis")
+    @GetMapping(value = "/tologin")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping(value = "/success")
+    public String success(){
+        return "success";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/mylogin")
+    public Result mylogin(@RequestParam(value = "name")String name){
+        log.info("===="+name);
+        Result result=new Result();
+        if(StringUtils.isBlank(name)){
+            result.setSuccess(false);
+            result.setMessage("用户名为空");
+            return result;
+        }else{
+            if("123".equals(name)){
+                result.setSuccess(true);
+                return result;
+            }else{
+                result.setSuccess(false);
+                result.setMessage("用户名错误");
+                return result;
+            }
+        }
+    }
+
+    @GetMapping(value = "/redis")
     public String myRedis(){
         String name="monkey";
         String s = JSON.toJSONString(name);
@@ -37,7 +68,7 @@ public class LoginController {
         return "success";
     }
 
-    @GetMapping(value = "getvalue")
+    @GetMapping(value = "/getvalue")
     public String getRedisKey(){
         log.info("我被调用了。。。");
         String value = stringRedisTemplate.opsForValue().get("wwwsssaaaddd");
