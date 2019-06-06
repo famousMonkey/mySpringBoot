188d6006242eae193673c0318a440c817595f421
monkey <13838121279@163.com>
2019-06-06 16:47:36 +0800

    获取用户的ip地址

--- demo/src/main/java/com/song/demo/controller/LoginController.java
+++ demo/src/main/java/com/song/demo/controller/LoginController.java
@@ -20,6 +20,53 @@ public class LoginController {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    @Autowired
+    private HttpServletRequest request;
+
+    @ResponseBody
+    @GetMapping(value = "/getip")
+    public String getIpAddress() {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+            // 多次反向代理后会有多个ip值，第一个ip才是真实ip
+            if( ip.indexOf(",")!=-1 ){
+                ip = ip.split(",")[0];
+            }
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+            log.info("Proxy-Client-IP ip: {}", ip);
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+            log.info("WL-Proxy-Client-IP ip: {}", ip);
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+            log.info("HTTP_CLIENT_IP ip: {}", ip);
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            log.info("HTTP_X_FORWARDED_FOR ip: {}", ip);
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+            log.info("X-Real-IP ip: {}", ip);
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            log.info("getRemoteAddr ip: {}", ip);
+        }
+        return ip;
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/allpay")
+    public String allPay(){
+        log.info("===== \n"+request.getHeader("user-agent")+"\n===== ");
+        return request.getHeader("user-agent");
+    }
+
     @ResponseBody
     @GetMapping(value = "/index")
     public String Login(HttpServletRequest request){
